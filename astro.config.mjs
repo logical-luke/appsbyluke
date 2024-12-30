4d67d5c05e125a1c45d2b9fc7141f58c0215f50e
logical-luke <luke@logical-luke.com>
2024-12-30 17:00:11 +0100

    :mag: Add autogenerated sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,10 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  site: 'https://appsbyluke.com',
+  integrations: [tailwind(), sitemap()]
 });
