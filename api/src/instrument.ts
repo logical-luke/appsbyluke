6d339d2d5be68bf13b8772e87eea6f81fd68dec5
logical-luke <luke@logical-luke.com>
2025-01-22 15:09:30 +0100

    :green_heart: Fix setting Sentry DSN

--- api/src/instrument.ts
+++ api/src/instrument.ts
@@ -1,16 +1,22 @@
 import * as Sentry from "@sentry/nestjs";
 import * as dotenv from 'dotenv';
 import * as path from 'path';
+import * as fs from "node:fs";
 
 const envPath = path.resolve(__dirname, '..', '.env');
 console.log('Attempting to load .env file from:', envPath);
 
-const result = dotenv.config({ path: envPath });
-if (result.error) {
-  console.error('Error loading .env file:', result.error);
+if (fs.existsSync(envPath)) {
+  const result = dotenv.config({ path: envPath });
+  if (result.error) {
+    console.error('Error loading .env file:', result.error);
+  } else {
+    console.log('.env file loaded successfully');
+  }
 } else {
-  console.log('.env file loaded successfully');
+  console.warn('.env file does not exist, skipping dotenv configuration');
 }
+
 console.log('SENTRY_DSN:', process.env.SENTRY_DSN);
 
 Sentry.init({
